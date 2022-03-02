9da7a31d7908fa28463cdac049f84530cac9e9b7
shepard <eviltomorrow@163.com>
2022-03-02 09:15:09 +0800

    mod trunc func

--- pkg/zmath/trunc.go
+++ pkg/zmath/trunc.go
@@ -2,9 +2,21 @@ package zmath
 
 import "math"
 
-var n10 = math.Pow10(2)
+var (
+	n10_1 = math.Pow10(2)
+	n10_4 = math.Pow10(4)
+)
 
 // Trunc2 trunc2
 func Trunc2(val float64) float64 {
-	return math.Trunc((val+0.5/n10)*n10) / n10
+	return math.Trunc((val+0.5/n10_1)*n10_1) / n10_1
+}
+
+func Trunc4(val float64) float64 {
+	return math.Trunc((val+0.5/n10_4)*n10_4) / n10_4
+}
+
+func TruncN(f float64, n int) float64 {
+	pow10_n := math.Pow10(n)
+	return math.Trunc((f+0.5/pow10_n)*pow10_n) / pow10_n
 }
